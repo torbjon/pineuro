9bd714c049b541cf5313347148b2a81d806e7bba
Eriks Remess <eriks@remess.lv>
2012-05-26 21:24:38 +0300

    dasdas

--- search.php
+++ search.php
@@ -204,8 +204,8 @@
 		var options = {
 			autoResize: true, // This will auto-update the layout when the browser window is resized.
 			container: $('#main'), // Optional, used for some extra CSS styling
-			offset: 4, // Optional, the distance between grid items
-			itemWidth: 210 // Optional, the width of a grid item
+			offset: 4//, // Optional, the distance between grid items
+			//itemWidth: 210 // Optional, the width of a grid item
 		};
 
 		/**
