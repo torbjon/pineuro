15eb675561822d0ba3408d42a20b578a700ea9e6
Eriks Remess <eriks@remess.lv>
2012-05-26 20:39:13 +0300

    fancyyyy

--- search.php
+++ search.php
@@ -174,7 +174,9 @@
 			$("#tiles").on("click", "img", function(){
 				$("#popup_img").css("background-image", 'url("http://social.apps.lv/image.php?cc=dedede&w=470&h=470&zc=2&src="'+$(this).data("imgsrc")+')')
 				$("#popup_img_title").html($(this).data("title"))
-				console.log($("#popup"))
+				$(".imagepopup").on("click", function(){
+					return false
+				})
 				$(".imagepopup").fancybox({
 					'width': 800,
 					'height': 500
