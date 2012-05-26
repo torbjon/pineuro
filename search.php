21d1d9982ca56f82f60bcc771e6a4b2fcbe165cc
Eriks Remess <eriks@remess.lv>
2012-05-26 20:51:50 +0300

    das

--- search.php
+++ search.php
@@ -172,7 +172,7 @@
 				})
 			}
 			$("#tiles").on("click", ".imagepopup", function(){
-				console.log($(this))
+				console.log($(this).data())
 				$("#popup_img").css('background-image', 'url(http://social.apps.lv/image.php?cc=dedede&w=470&h=470&zc=2&src='+$(this).data("imgsrc")+')')
 				$("#popup_img_title").html($(this).data("title"))
 				$(".imagepopup").fancybox({
