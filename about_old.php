f2d61a76b5a79720d2aba2917b9c9e7e935c6736
Eriks Remess <eriks@remess.lv>
2012-05-27 15:17:07 +0300

    about

--- about_old.php
+++ about_old.php
@@ -32,13 +32,6 @@
 				<li>Share the image with friends using "Pin It" and "Facebook Like" buttons.</li>
 			</ul>
 
-			<p>Explore by following these sample queries:</p>
-			<ul>
-				<li><a href="http://europ.in/search?q=da%20Vinci">Search for: da Vinci</a></li>
-				<li><a href="http://europ.in/search?q=flowers">Search for: flowers</a></li>
-				<li><a href="http://europ.in/search?q=R%C4%ABga">Search for: Rīga</a></li>
-			</ul>
-
 			<p>Europ.in was developed as a part of the #Hack4Europe Riga hackaton by:</p>
 			<ul>
 				<li><a href="https://www.facebook.com/eriks.remess" target="_blank">Ēriks Remess</a></li>
@@ -46,10 +39,16 @@
 				<li><a href="https://www.facebook.com/uldis.bojars" target="_blank">Uldis Bojars</a></li>
 			</ul>
 
+			<h2>Technical info</h2>
+
+			<p><i>... te var ielikt sīkāku info par to kā projekts strādā (kādas lietas meklē, kādas atmet) - arī info par mūsu atziņām strādājot ar europeana (tas gan labāk iet blog ierakstos)</i></p>
+
 			<h2>Links</h2>
 
+			<p><i>... te linki uz Hack4Europe 2012, blog ierakstiem par projektu, GitHub</i></p>
+
 			<ul>
-				<li><a href="https://github.com/EriksRemess/pineuro">Europ.in code on GitHub</li>
+				<li>Eriks</li>
 				<li><a href="http://pro.europeana.eu/web/guest/hack4europe-2012">Hack4Europe! 2012 hackaton</a></li>
 				<li><a href="http://www.biblioteka.lv/Libraries/latvijas-nacionala-biblioteka/News/ArticleItem.aspx?article=17889&type=0">Hack4Europe! 2012 in Riga (in Latvian)</a></li>
 			</ul>
