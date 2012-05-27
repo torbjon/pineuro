50786b63c55e5c303c8cfaf8d11bfe6397a020a2
Eriks Remess <eriks@remess.lv>
2012-05-27 05:28:00 +0300

    replaceState?

--- assets/js/app.js
+++ assets/js/app.js
@@ -74,7 +74,7 @@ $(function(){
 		})
 	}
 	$("#tiles").on("click", ".imagepopup", function(){
-		history.pushState(null, null, "item/"+$(this).children("img").data("originaluri").replace("http://www.europeana.eu/resolve/record/","")+"?q="+encodeURIComponent(searchTerm))
+		history.replaceState(null, null, "item/"+$(this).children("img").data("originaluri").replace("http://www.europeana.eu/resolve/record/","")+"?q="+encodeURIComponent(searchTerm))
 		$("#popup_img").css('background-image', 'url(http://social.apps.lv/image.php?cc=333&w=470&h=470&zc=2&src='+encodeURIComponent($(this).children("img").data("imgsrc"))+')')
 		if($(this).children("img").data("title") != undefined){
 			$("#popup_img_title").html($(this).children("img").data("title"))
