03a96452f72597262c6a74822daaaa37fda90a80
Eriks Remess <eriks@remess.lv>
2012-05-27 14:00:53 +0300

    das

--- request.php
+++ request.php
@@ -2,7 +2,7 @@
 define("CACHE_PATH", $_SERVER['DOCUMENT_ROOT']."/cache/");
 define("CACHE_URL_PATH", "/cache/");
 function getjson(){
-	$params = http_build_query(array_merge($_GET, array("wskey"=>"HTMQFSCKKB"));
+	$params = http_build_query(array_merge($_GET, array("wskey"=>"HTMQFSCKKB")));
 	echo $params;
 }
 getjson();
