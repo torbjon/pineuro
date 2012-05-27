a65951e4140ffbe9df8403b001cd22204652211d
Maksim Berjoza <torbjon@gmail.com>
2012-05-27 13:02:13 +0300

    europeana about new design

---
+++ about.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>About - Europ.in - explore and share Europeana</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <link rel="stylesheet" href="/assets/css/reset.css" />
+  <link rel="stylesheet" href="/assets/css/style.css" />
+  <link rel="stylesheet" href="/assets/css/bootstrap.css" />
+</head>
+<body>
+  <div id="header">
+    <a href="/"><img src="/assets/images/logo_small.png" height="30" width="118" alt="" class="logo"></a>
+    <form id="f" action="/search" class="search">
+      <input type="text" id="q" name="q" value="" class="search_bar" />
+      <button id="s" class="btn btn-small">Search</button>
+    </form>
+    <div id="count"></div>
+  </div>
+  <div id="main" class="clearfix">
+    <div class="modal_about">
+
+      <h1>About Europ.in</h1>
+
+      <p>Europ.in is a service for the visual discover and exploration of Europeana content.</p>
+
+      <p>It starts with a simple search screen:</p>
+      <ul>
+        <li>Enter search keywords and watch as images from Europeana appear before you in a continuous stream.</li>
+        <li>The item view shows detailed information about the image along with <i>social sharing</i> buttons and a link to this item on Europeana.</li>
+        <li>Share the image with friends using "Pin It" and "Facebook Like" buttons.</li>
+      </ul>
+
+      <p>Europ.in was developed as a part of the #Hack4Europe Riga hackaton by:</p>
+      <ul>
+        <li>Eriks</li>
+        <li>Maksim</li>
+        <li>Uldis</li>
+      </ul>
+
+      <h2>Technical info</h2>
+
+      <p><i>... te var ielikt sīkāku info par to kā projekts strādā (kādas lietas meklē, kādas atmet) - bet nav obligāti ... - var arī info par mūsu atziņām strādājot ar europeana (tas gan labāk iet blog ierakstos)</i></p>
+
+      <h2>Links</h2>
+
+      <p><i>... te linki uz Hack4Europe 2012, blog ierakstiem par projektu, GitHub</i></p>
+
+    </div>
+  </div>
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+</body>
+</html>
