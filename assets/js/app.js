ab4ddbc8fb32779442b22955a8b6489b5e473289
Eriks Remess <eriks@remess.lv>
2012-05-27 11:42:17 +0300

    fbb

--- assets/js/app.js
+++ assets/js/app.js
@@ -91,7 +91,8 @@ $(function(){
 			}
 			$("#pinbutton").html(
 				'<a href="http://pinterest.com/pin/create/button/?url='+encodeURIComponent(document.location.href)+'&media='+encodeURIComponent($(this).children("img").data("imgsrc"))+'&description='+encodeURIComponent($(this).children("img").data("title"))+'" class="pin-it-button" count-layout="vertical"><img border="0" src="//assets.pinterest.com/images/PinExt.png" title="Pin It" /></a><script type="text/javascript" src="http://assets.pinterest.com/js/pinit.js"><'+'/script>'+
-				'<div id="fb-root"></div><script src="//connect.facebook.net/en_US/all.js#xfbml=1&appId=130236337100576"></'+'script>');
+				'<div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#xfbml=1&appId=130236337100576"></'+'script>'
+			);
 		} else {
 			$("#popup_img_title").html("")
 		}
