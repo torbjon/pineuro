4c8b39f5ec50fc9ec2a9962f0891410c5a26ee9d
Eriks Remess <eriks@remess.lv>
2012-05-27 12:56:43 +0300

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -49,10 +49,11 @@ function load_images(options){
 					newimg = new Image()
 					newimg.src = "http://social.apps.lv/image.php?w=196&zc=2&src="+encodeURIComponent(item['europeana:object'])
 					newimg.onload = function(){
-							var subjects = []
-							$.each(item['dc:subject'], function(i){
-								subjects.push("<a href='/?q="+encodeURIComponent(item['dc:subject'][i])+"'>"+item['dc:subject'][i]+"</a>")
-							})
+						var subjects = []
+						$.each(item['dc:subject'], function(i){
+							subjects.push("<a href='/search?q="+encodeURIComponent(item['dc:subject'][i])+"'>"+item['dc:subject'][i]+"</a>")
+						})
+						console.log(subjects)
 						//if(this.width == 200){
 							$("#tiles").append(
 								"<li><a class='imagepopup' href='#popup'><img width='"+this.width+
@@ -117,7 +118,7 @@ $(function(){
 		return false
 	})
 	$("body").on("click", "#fancybox-close", function(){
-		history.pushState(null, null, "/?q="+encodeURIComponent(searchTerm))
+		history.pushState(null, null, "/search?q="+encodeURIComponent(searchTerm))
 	})
 })
 String.prototype.capitalize = function(){
