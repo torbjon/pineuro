0ebe31027e40ba3725b2981d1c8ad667939d2e77
Eriks Remess <eriks@remess.lv>
2012-05-27 12:16:10 +0300

    test

--- search.php
+++ search.php
@@ -101,7 +101,7 @@ endif;
 						'<a href="http://pinterest.com/pin/create/button/?url='+encodeURIComponent(document.location.href)+'&media='+encodeURIComponent(item['europeana:object'].replace(/\s/g,"%20"))+'&description='+encodeURIComponent(item['dc:title'].toString())+'" class="pin-it-button" count-layout="vertical"><img border="0" src="//assets.pinterest.com/images/PinExt.png" title="Pin It" /></a><script type="text/javascript" src="http://assets.pinterest.com/js/pinit.js"><'+'/script>'+
 						'<div class="fb-like" data-href="'+encodeURIComponent(document.location.href)+'" data-send="false" data-layout="box_count" data-width="450" data-show-faces="false"></div>'
 					);
-					FB.XFBML.parse()
+					FB.XFBML.parse($("#pinbutton"))
 				} else {
 					$("#popup_img_title").html("")
 				}
