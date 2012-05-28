6a56702184f3fff3e4b184e59aa463bb4634168b
Maksim Berjoza <torbjon@gmail.com>
2012-05-29 01:48:05 +0300

    fixed fancy onClose and rewrited ga script

--- search.php
+++ search.php
@@ -126,7 +126,10 @@ endif;
 						'transitionIn'	: 'elastic',
 						'transitionOut'	: 'elastic',
 						'easingIn'		: 'easeOutBack',
-						'easingOut'		: 'easeInBack'
+						'easingOut'		: 'easeInBack',
+						'onClosed'		: function() {
+	    					history.pushState(null, null, "/search?q="+encodeURIComponent(searchTerm))
+						}
 					}
 				)
 			})
@@ -135,16 +138,12 @@ endif;
 <?
 	endif;
 ?>
-<script>
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-32161057-1']);
-	_gaq.push(['_trackPageview']);
 
-	(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
+<script>
+	var _gaq=[['_setAccount','UA-32161057-1'],['_trackPageview']];
+	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+	s.parentNode.insertBefore(g,s)}(document,'script'));
 </script>
 </body>
 </html>
